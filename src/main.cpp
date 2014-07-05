2e8a4bf7f89f3bdbb945d5ac3ddfbb7c5c66c3b2
Alex Borg <junior.idiot.savant@gmail.com>
2014-07-05 16:33:39 -0500

    correct gitignore to keep 3rdparty libs

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "BBBiolib.h"
+#include <sys/time.h>
+#include <fstream>
+#include <string>
+
+using namespace std ;
+
+int main () {
+  cout << "hello world" << endl ;
+
+  return 0 ;
+}
