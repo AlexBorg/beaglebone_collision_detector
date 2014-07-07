cd7516272d2d0de7b02702e650b2dca5fe06d874
Alex Borg <junior.idiot.savant@gmail.com>
2014-07-07 15:58:23 -0500

    give up on cross compilation and commit main with example threads and message passing

--- README.md
+++ README.md
@@ -8,14 +8,12 @@ A project to accept input from stereoscopic cameras in order to determine if obj
 ### Compiling
 It is highly reccomended that you create a build subdirectory at the top level of the source and run cmake from there.
 
-On the beagleboard: requires cmake
+On the beagleboard: requires cmake, opencv, and xenomai
 	cmake <target source dir>
 	make
 
-On x86 Linux system (tested using ubuntu 14.4): requires cmake, gnueabihf, and copy of compiled xenomai
+options that can be added:
 	cmake -DGCC_COMPILER_VERSION=4.8 -DXENOMAI_BASE_DIR=<copy of /usr/xenomai> <target source dir>
-	make
-replace 4.8 with the version of the gnueabihf compiler installed
 
 ### authors
 Alex Borg
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <string>
 #include <unistd.h>
 #include <native/task.h>
+#include <native/queue.h>
+
+#include "opencv2/opencv.hpp"
+#include "cv.h"
 
 #define TASK_PRIO  99 /* Highest RT priority */
 #define TASK_MODE  0  /* No flags */
@@ -11,16 +15,30 @@
 
 using namespace std ;
 
+// simple thread operation.
+// it receives data from a queue, casts it to an opencv data type and prints data from it.
 void thread2 (void *cookie)
 {
+  RT_QUEUE first_queue ;
+  int err = rt_queue_bind (&first_queue, "first queue", TM_INFINITE) ;
+
+  cout << "thread2 got queue" << endl ;
   int loop = 0 ;
+  void* msg ;
+  size_t len ;
   while ( loop < 10 ) {
     ++loop ;
     cout << "thread2 loop " << loop << endl ;
-    sleep ( 1 ) ;
+    len = rt_queue_receive ( &first_queue , &msg , TM_INFINITE ) ;
+    if ( len == 0 )
+      continue ;
+    CvMat* img_ptr ;
+    memcpy ( &img_ptr , msg , len ) ;
+    cout << "recv dimention " << img_ptr -> width << "x" << img_ptr -> height << endl ;
   }
 }
 
+// simple example thread operation. it just prints the loop count and sleeps 
 void thread3 (void *cookie)
 {
   int loop = 0 ;
@@ -34,6 +52,16 @@ void thread3 (void *cookie)
 int main () {
   cout << "hello world" << endl ;
   int err ;
+
+  // create a queue.
+  // TODO: Borg: Because we are all in the same executable, would it be easier to mutex
+  //   a standard queue?
+  RT_QUEUE first_queue ;
+  err = rt_queue_create (&first_queue, "first queue", 100, Q_UNLIMITED, Q_FIFO | Q_SHARED) ;
+  
+  // create thread example.
+  // TODO: Borg: replace with an object that owns it's own thread. We will have to pass
+  // the queue to that object.
   RT_TASK thread2_desc ;
   err = rt_task_create(&thread2_desc,
                      "thread 2",
@@ -52,12 +80,29 @@ int main () {
   if (!err)
     rt_task_start(&thread3_desc,&thread3,NULL);
 
+
+  //test read input images
+  const char* imgfilename1 = "tsukuba1color.png" ;
+  const char* imgfilename2 = "tsukuba2color.png" ;
+  cv::Mat img1 = cv::imread(imgfilename1, CV_LOAD_IMAGE_COLOR);
+  cv::Mat img2 = cv::imread(imgfilename2, CV_LOAD_IMAGE_COLOR);
+
   int loop = 0 ;
   while ( loop < 10 ) {
     ++loop ;
     cout << "main thread loop " << loop << endl ;
+    CvMat* img_ptr = cvLoadImageM(imgfilename1, CV_LOAD_IMAGE_COLOR); // simulate new image
+    cout << "send dimention " << img_ptr -> width << "x" << img_ptr -> height << endl ;
+    int msg_sz = sizeof ( CvMat* ) ;
+    // push the pointer to the queue. we no longer have ownership of the pointer
+    rt_queue_write (  &first_queue , &img_ptr , msg_sz , Q_NORMAL );
     sleep ( 1 ) ;
   }
+  
+  rt_queue_write (  &first_queue , NULL , 0 , Q_NORMAL ); // empty message to signal end
+
 
   return 0 ;
 }
+
+
